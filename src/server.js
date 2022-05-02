c5c06b756270b465f275053804b1ac849ad12274
Lucai994 <luca.muratori.94@hotmail.it>
2022-05-02 14:38:07 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
-import listendpoints from "express-list-endpoints";
+import listEndpoints from "express-list-endpoints";
 import experiencesRouter from "./services/experiences/experienceIndex.js";
+import profileRouter from "./services/profiles/index.js";
 
 const server = express();
 const PORT = process.env.PORT || 3002;
 
 server.use("/experiences", experiencesRouter);
+server.use("/profile", profileRouter);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  console.table(listendpoints(server));
+  console.table(listEndpoints(server));
 });
