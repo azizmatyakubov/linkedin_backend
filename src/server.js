a945453477145ffa3de1aa05651554471ea55243
Lucai994 <luca.muratori.94@hotmail.it>
2022-05-02 12:50:49 +0200

    commit

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-import express from 'express'
-import listendpoints from 'express-list-endpoints'
+import express from "express";
+import listendpoints from "express-list-endpoints";
+import experiencesRouter from "./experiences/experienceIndex.js";
 
-const server = express()
-const PORT = process.env.PORT || 3002
+const server = express();
+const PORT = process.env.PORT || 3002;
 
+<<<<<<< Updated upstream
 console.log(PORT)
 
 
@@ -11,3 +13,11 @@ server.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`)
     console.table(listendpoints(server))
 })
+=======
+server.use("/experiences", experiencesRouter);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.table(listendpoints(server));
+});
+>>>>>>> Stashed changes
