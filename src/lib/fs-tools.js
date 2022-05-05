338b682674e2465bbe9e1137f10a3199debb58e1
Lucai994 <luca.muratori.94@hotmail.it>
2022-05-05 15:14:48 +0200

    commit

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,25 +1,19 @@
-import fs from "fs-extra"
-import { fileURLToPath } from "url"
-import { join, dirname } from "path"
+import fs from "fs-extra";
+import { fileURLToPath } from "url";
+import { join, dirname } from "path";
 
-const { readJSON, writeJSON, writeFile } = fs
+const { readJSON, writeJSON, writeFile, createReadStream } = fs;
 
 //process.cwd() return the folder of this project
 
-// fs.ensureDirSync to be sure to create the path/folder
+// const experiencesFolderPath = join(
+//   process.cwd(),
+//   "../../public/imageFolder/experience"
+// );
 
-const experiencesFolderPath = join(process.cwd(), "../../public/imageFolder/experience")
+// console.log(experiencesFolderPath);
 
-// const profileAvatarFolderPath = join(process.cwd(), "./public/profile/avatar")
-// fs.ensureDirSync(profileAvatarFolderPath) // it partially works, first time crashes the app, but creates the PATH
-
-console.log(experiencesFolderPath)
-
-//save the picture in the public folder
-export const saveExperiencesImage = (fileName, contentAsBuffer) => {
-  writeFile(join(experiencesFolderPath, fileName), contentAsBuffer)
-}
-//save the profile's avatar in the public folder
-export const saveProfileAvatar = (fileName, contentAsBuffer) => {
-  writeFile(join(profileAvatarFolderPath, fileName), contentAsBuffer)
-}
+// //save the picture in the public folder
+// export const saveExperiencesImage = (fileName, contentAsBuffer) => {
+//   writeFile(join(experiencesFolderPath, fileName), contentAsBuffer);
+// };
--- src/services/profiles/index.js
+++ src/services/profiles/index.js
@@ -1,20 +1,19 @@
-import express from "express"
-import createError from "http-errors"
-import profileSchema from "./model.js"
-import { pipeline } from "stream"
-import experienceSchema from "../experiences/model.js"
-import q2m from "query-to-mongo"
+import express from "express";
+import createError from "http-errors";
+import profileSchema from "./model.js";
+import { pipeline } from "stream";
+import experienceSchema from "../experiences/model.js";
+import q2m from "query-to-mongo";
 //-----for creating and upload a new image
-import multer from "multer"
-import { saveExperiencesImage } from "../../lib/fs-tools.js"
-import { CloudinaryStorage } from "multer-storage-cloudinary"
-import { v2 as cloudinary } from "cloudinary"
-import { getPdfReadableStream } from "../../lib/pdf-tools.js"
-import json2csv from "json2csv"
-import fs from "fs-extra"
-import csv from "csv-express"
+import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import { getPdfReadableStream } from "../../lib/pdf-tools.js";
+import json2csv from "json2csv";
+import fs from "fs-extra";
+import csv from "csv-express";
 
-const profileRouter = express.Router()
+const profileRouter = express.Router();
 
 const cloudinaryAvatarUploader = multer({
   storage: new CloudinaryStorage({
@@ -23,7 +22,7 @@ const cloudinaryAvatarUploader = multer({
       folder: "profile_avatars",
     },
   }),
-}).single("avatar")
+}).single("avatar");
 
 //CREATE ERRORS
 // finish pdf enpoint
@@ -31,244 +30,284 @@ const cloudinaryAvatarUploader = multer({
 ////////////
 profileRouter.post("/", async (req, res, next) => {
   try {
-    const profile = new profileSchema(req.body)
+    const profile = new profileSchema(req.body);
 
-    const { _id } = await profile.save()
+    const { _id } = await profile.save();
 
-    res.status(201).send(_id)
+    res.status(201).send(_id);
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 ///////////
 
 //  POST https://yourapi.herokuapp.com/api/profile/{userId}/picture
 // Replace user profile picture (name = profile)
-profileRouter.put("/:profileId/uploadAvatar", cloudinaryAvatarUploader, async (req, res, next) => {
-  try {
-    const profile = await profileSchema.findByIdAndUpdate(req.params.profileId, { image: req.file.path }, { new: true })
+profileRouter.put(
+  "/:profileId/uploadAvatar",
+  cloudinaryAvatarUploader,
+  async (req, res, next) => {
+    try {
+      const profile = await profileSchema.findByIdAndUpdate(
+        req.params.profileId,
+        { image: req.file.path },
+        { new: true }
+      );
 
-    if (profile) {
-      res.status(201).send(profile)
-    } else {
-      console.log("profile not found")
+      if (profile) {
+        res.status(201).send(profile);
+      } else {
+        console.log("profile not found");
+      }
+    } catch (error) {
+      console.log(error);
+      next(error);
     }
-  } catch (error) {
-    console.log(error)
-    next(error)
   }
-})
+);
 
 ////////////
 profileRouter.get("/", async (req, res, next) => {
   try {
-    const profile = await profileSchema.find()
-    res.status(200).send(profile)
+    const profile = await profileSchema.find();
+    res.status(200).send(profile);
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 ////////////
 profileRouter.get("/:profileId", async (req, res, next) => {
   try {
-    const profile = await profileSchema.findById(req.params.profileId).populate("experiences")
+    const profile = await profileSchema
+      .findById(req.params.profileId)
+      .populate("experiences");
     if (profile) {
-      res.status(200).send(profile)
+      res.status(200).send(profile);
     } else {
-      console.log("This profile does not exist")
+      console.log("This profile does not exist");
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 //  GET https://yourapi.herokuapp.com/api/profile/{userId}/CV
 // Generates and download a PDF with the CV of the user (details, picture, experiences)
 profileRouter.get("/:profileId/downloadCV", async (req, res, next) => {
   try {
-    const profile = await profileSchema.findById(req.params.profileId)
+    const profile = await profileSchema.findById(req.params.profileId);
     //do a loop to retrieve every experiences ... toString/toObject
-    if (profile) { 
+    if (profile) {
+      //   const pdfStream = await generateBlogPDF(blog);
 
-    //   const pdfStream = await generateBlogPDF(blog);
-
-    //res.setHeader("Content-Type", "application/pdf");
-    // pipeline(pdfStream, res, (err) => {
-    //   if (err) {
-    //     console.log(err);
-    //   }
-    // });
-    //pdfStream.end();
-    res.setHeader("Content-Type", "application/pdf");
-      const source =await  getPdfReadableStream(profile)
-      const destination = res
+      //res.setHeader("Content-Type", "application/pdf");
+      // pipeline(pdfStream, res, (err) => {
+      //   if (err) {
+      //     console.log(err);
+      //   }
+      // });
+      //pdfStream.end();
+      res.setHeader("Content-Type", "application/pdf");
+      const source = await getPdfReadableStream(profile);
+      const destination = res;
 
       pipeline(source, destination, (err) => {
-        if (err) console.log(err)
-      })
-     // source.end()
+        if (err) console.log(err);
+      });
+      // source.end()
     } else {
-      console.log("this profile does not exist")
+      console.log("this profile does not exist");
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 ////////////
 profileRouter.put("/:profileId", async (req, res, next) => {
   try {
-    const profile = await profileSchema.findByIdAndUpdate(req.params.profileId, req.body, { new: true })
+    const profile = await profileSchema.findByIdAndUpdate(
+      req.params.profileId,
+      req.body,
+      { new: true }
+    );
     if (profile) {
-      res.status(200).send(profile)
+      res.status(200).send(profile);
     } else {
-      console.log("This profile does not exist")
+      console.log("This profile does not exist");
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 ////////////
 profileRouter.delete("/:profileId", async (req, res, next) => {
   try {
-    const profile = await profileSchema.findByIdAndDelete(req.params.profileId)
+    const profile = await profileSchema.findByIdAndDelete(req.params.profileId);
     if (profile) {
-      res.status(200).send("Profile Destroyed")
+      res.status(200).send("Profile Destroyed");
     } else {
-      console.log("This profile does not exist")
+      console.log("This profile does not exist");
     }
   } catch (error) {
-    console.log(error)
-    next(error)
+    console.log(error);
+    next(error);
   }
-})
+});
 
 //----------------------------------creating the endponits for the experience
 profileRouter.post("/:username/experiences", async (req, res, next) => {
   try {
-    const user = await profileSchema.find({ _id: req.params.username })
+    const user = await profileSchema.find({ _id: req.params.username });
     if (user) {
       const experienceToInsert = await experienceSchema({
         ...req.body,
         user: req.params.username,
-      }).save()
+      }).save();
 
       const modifiedUser = await profileSchema.findOneAndUpdate(
         { _id: req.params.username },
         { $push: { experiences: experienceToInsert } },
         { new: true, runValidators: true }
-      )
+      );
 
       if (modifiedUser) {
-        res.send(modifiedUser)
+        res.send(modifiedUser);
       }
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 profileRouter.get("/:username/experiences", async (req, res, next) => {
   try {
-    const user = await profileSchema.findById(req.params.username).populate("experiences")
-    console.log(user.experiences)
+    const user = await profileSchema
+      .findById(req.params.username)
+      .populate("experiences");
+    console.log(user.experiences);
     if (user) {
-      res.send(user.experiences)
+      res.send(user.experiences);
     } else {
-      next(createError(404, `Blog post with ${req.params.blogPostId} not found`))
+      next(
+        createError(404, `Blog post with ${req.params.blogPostId} not found`)
+      );
     }
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
-profileRouter.get("/:userName/experiences/:experienceId", async (req, res, next) => {
-  try {
-    const user = await profileSchema.findById(req.params.userName).populate("experiences")
-    if (user) {
-      const experience = await user.experiences.find((experience) => req.params.experienceId === experience._id.toString()).populate("user")
-      console.log(user.experiences)
-      if (experience) {
-        res.send(experience)
-      } else {
-        next(createError(404, "Experience not found"))
-      }
-      await user.save()
-      res.send(user)
-    } else {
-      next(createError(404, "Blog post not found"))
-    }
-  } catch (error) {
-    next(error)
-  }
-})
-
-profileRouter.delete("/:userName/experiences/:experienceId", async (req, res, next) => {
-  try {
-    const modifiedUser = await profileSchema.findOneAndDelete(
-      req.params.userName,
-      {
-        $pull: { experiences: { _id: req.params.experienceId } },
-      },
-      { new: true }
-    )
-
-    if (modifiedUser) {
-      res.send(modifiedUser)
-    } else {
-      next(createError(404, "blogPost not found"))
-    }
-  } catch (error) {
-    console.log(error)
-  }
-})
-
-profileRouter.put("/:userName/experiences/:experienceId", async (req, res, next) => {
-  try {
-    const user = await profileSchema.findById(req.params.userName).populate("experiences")
-    if (user) {
-      const index = user.experiences.findIndex((experience) => experience._id.toString() === req.params.experienceId)
-      if (index !== -1) {
-        user.experiences[index] = {
-          ...user.experiences[index],
-          ...req.body,
+profileRouter.get(
+  "/:userName/experiences/:experienceId",
+  async (req, res, next) => {
+    try {
+      const user = await profileSchema
+        .findById(req.params.userName)
+        .populate("experiences");
+      if (user) {
+        const experience = await user.experiences
+          .find(
+            (experience) =>
+              req.params.experienceId === experience._id.toString()
+          )
+          .populate("user");
+        console.log(user.experiences);
+        if (experience) {
+          res.send(experience);
+        } else {
+          next(createError(404, "Experience not found"));
         }
-        await user.save()
-        res.send(user)
+        await user.save();
+        res.send(user);
       } else {
-        next(createError(404, "comment not found"))
+        next(createError(404, "Blog post not found"));
       }
-    } else {
-      next(createError(404, "Blog post not found"))
+    } catch (error) {
+      next(error);
     }
-  } catch (error) {
-    next(error)
   }
-})
+);
 
-profileRouter.delete("/:userName/experiences/:experienceId", async (req, res, next) => {
-  try {
-    const modifiedUser = await profileSchema.findByIdAndUpdate(
-      req.params.userName,
-      {
-        $pull: {
-          experiences: req.params.experienceId,
+profileRouter.delete(
+  "/:userName/experiences/:experienceId",
+  async (req, res, next) => {
+    try {
+      const modifiedUser = await profileSchema.findOneAndDelete(
+        req.params.userName,
+        {
+          $pull: { experiences: { _id: req.params.experienceId } },
         },
-      },
-      { new: true }
-    )
-    if (modifiedUser) {
-      res.send(modifiedUser)
+        { new: true }
+      );
+
+      if (modifiedUser) {
+        res.send(modifiedUser);
+      } else {
+        next(createError(404, "blogPost not found"));
+      }
+    } catch (error) {
+      console.log(error);
     }
-  } catch (error) {
-    res.send(error)
   }
-})
+);
+
+profileRouter.put(
+  "/:userName/experiences/:experienceId",
+  async (req, res, next) => {
+    try {
+      const user = await profileSchema
+        .findById(req.params.userName)
+        .populate("experiences");
+      if (user) {
+        const index = user.experiences.findIndex(
+          (experience) => experience._id.toString() === req.params.experienceId
+        );
+        if (index !== -1) {
+          user.experiences[index] = {
+            ...user.experiences[index].toObject(),
+            ...req.body,
+          };
+          await user.save();
+          res.send(user);
+        } else {
+          next(createError(404, "comment not found"));
+        }
+      } else {
+        next(createError(404, "Blog post not found"));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+profileRouter.delete(
+  "/:userName/experiences/:experienceId",
+  async (req, res, next) => {
+    try {
+      const modifiedUser = await profileSchema.findByIdAndUpdate(
+        req.params.userName,
+        {
+          $pull: {
+            experiences: req.params.experienceId,
+          },
+        },
+        { new: true }
+      );
+      if (modifiedUser) {
+        res.send(modifiedUser);
+      }
+    } catch (error) {
+      res.send(error);
+    }
+  }
+);
 
 //---------------------------------Change the experience picture
 const cloudinaryUploader = multer({
@@ -276,37 +315,48 @@ const cloudinaryUploader = multer({
     cloudinary,
     params: { folder: "experienceLinkedinImage" },
   }),
-}).single("expImage")
+}).single("expImage");
 
-profileRouter.put("/:userId/experiences/:expId/picture", cloudinaryUploader, async (req, res, next) => {
-  try {
-    const experience = await experienceSchema.findByIdAndUpdate(req.params.expId, { image: req.file.path }, { new: true })
-    res.send(experience)
-  } catch (error) {
-    next(error)
+profileRouter.put(
+  "/:userId/experiences/:expId/picture",
+  cloudinaryUploader,
+  async (req, res, next) => {
+    try {
+      const experience = await experienceSchema.findByIdAndUpdate(
+        req.params.expId,
+        { image: req.file.path },
+        { new: true }
+      );
+      res.send(experience);
+    } catch (error) {
+      next(error);
+    }
   }
-})
+);
 
 //---------------------------for downloading the csv
 
 profileRouter.get("/:userId/downloadExperiencesCSV", async (req, res, next) => {
   try {
-    var filename = "experiences.csv"
+    var filename = "experiences.csv";
 
     experienceSchema
       .find({ user: req.params.userId })
       .lean()
       .exec({}, function (err, experiences) {
-        if (err) res.send(err)
+        if (err) res.send(err);
 
-        res.statusCode = 200
-        res.setHeader("Content-Type", "text/csv")
-        res.setHeader("Content-Disposition", "attachment; filename=" + filename)
-        res.csv(experiences, true)
-      })
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/csv");
+        res.setHeader(
+          "Content-Disposition",
+          "attachment; filename=" + filename
+        );
+        res.csv(experiences, true);
+      });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-export default profileRouter
+export default profileRouter;
